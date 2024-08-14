4e24b648f7b2014ccdee4210bccfea36b2fc93c0
reanglee <gusdl199@naver.com>
2024-08-14 16:27:28 +0900

    240814 sql

--- java_work/ex23/src/main/java/org/example/DBMain.java
+++ java_work/ex23/src/main/java/org/example/DBMain.java
@@ -1,8 +1,34 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class DBMain {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        DBRepository dbRepository = new DBRepository();
+        while (true){
+            System.out.println("""
+                    뭐할래?
+                    1. 입력
+                    2. 조회
+                    3. 수정
+                    4. 삭제
+                    5. 종료""");
+            int cho = scanner.nextInt();
+            //insert 문 실행
+            if(cho==1){
+                dbRepository.insert();
+            }
+            //select 문 실행
+            else if (cho==2) {
+                dbRepository.select();
+            }else {
+                System.out.println("종료됩니다.");
+                break;
+            }
+        }
     }
 }
--- java_work/ex23/src/main/java/org/example/DBRepository.java
+++ java_work/ex23/src/main/java/org/example/DBRepository.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 public class DBRepository {
 
     //1. insert
@@ -8,7 +13,37 @@ public class DBRepository {
     //4. delete
 
     //insert
-    public void insert(){
+    public void insert() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("이름 입력");
+            String name = scanner.next();
+            System.out.println("나이 입력");
+            int age = scanner.nextInt();
 
+            //1. mysql jar 추가 확인
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //2.  db connection db연결확인
+            Connection conn =
+                    DriverManager
+                            .getConnection("jdbc:mysql://localhost:3306/pmh",
+                                    "root",
+                                    "12345678");
+            System.out.println("연결성공");
+            //3. sql 문 작성
+            PreparedStatement pstmt
+                    = conn.prepareStatement("INSERT INTO aa (name,age) VALUES (?,?)");
+            pstmt.setString(1,name);  //1번 물음표는 이름
+            pstmt.setInt(2,age);  //2번 물음표는 나이가 들어갈것이다
+            //4. sql 문 실행
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void select(){
+        System.out.println("select 해야함");
     }
 }
