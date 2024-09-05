e98f5133dac8f4457cbefd62ff4d1fa5cd52852f
raenglee <gusdl199@naver.com>
2024-09-05 17:27:31 +0900

    240905 vue 오후수업...

---
+++ vue_work/ex01/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>안녕</h1>
+<MyAA name="홍길동"></MyAA>
+<MyAA name="김길동"></MyAA>
+<MyAA name="이길동"></MyAA>
+
+  </div>
+</template>
+
+<script>
+//@는 jsconfig.jason에 적혀있는데, ./src/를 뜻하는 것 (소스파일 아래~)
+
+import MyAA from '@/components/MyAA.vue'
+
+export default {
+components:{
+  MyAA
+},
+setup () {
+    return {MyAA}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue_work/ex01/src/components/MyAA.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>MyAA = {{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['name'],
+  setup () {
+    // const name = '홍길동';
+    //  return {name}
+    return{}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
