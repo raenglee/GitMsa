745cde4480473e2995cd669aa2435fd13b16e27f
raenglee <gusdl199@naver.com>
2024-11-19 12:59:57 +0900

    241119

---
+++ msa_work/secondservice/src/test/java/com/green/firstservice/SecondserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
